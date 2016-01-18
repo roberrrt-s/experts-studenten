dc8be465b0d0fefc4be33d4b15d099745d745dac
roberrrt-s <robert.spier@gmail.com>
2016-01-18 11:08:39 +0100

    updated by heleen
    Heleen's update

--- js/main.js
+++ js/main.js
@@ -19,10 +19,24 @@ $('#techclick').click(function() {
 	if(tech) {
 		$('.tech').show()
 		tech = false;
+		$('#techclick').css({
+	     '-moz-transform':'rotate(0deg)',
+	     '-webkit-transform':'rotate(0deg)',
+	     '-o-transform':'rotate(0deg)',
+	     '-ms-transform':'rotate(0deg)',
+	     'transform':'rotate(0deg)'
+		});
 	}
 	else {
 		$('.tech').hide()
 		tech = true;
+		$('#techclick').css({
+	     '-moz-transform':'rotate(-90deg)',
+	     '-webkit-transform':'rotate(-90deg)',
+	     '-o-transform':'rotate(-90deg)',
+	     '-ms-transform':'rotate(-90deg)',
+	     'transform':'rotate(-90deg)'
+		});
 	}
 })
 
@@ -30,9 +44,23 @@ $('#designclick').click(function() {
 	if(design) {
 		$('.design').show()
 		design = false;
+		$('#designclick').css({
+	     '-moz-transform':'rotate(0deg)',
+	     '-webkit-transform':'rotate(0deg)',
+	     '-o-transform':'rotate(0deg)',
+	     '-ms-transform':'rotate(0deg)',
+	     'transform':'rotate(0deg)'
+		});
 	}
 	else {
 		$('.design').hide()
 		design = true;
+		$('#designclick').css({
+	     '-moz-transform':'rotate(-90deg)',
+	     '-webkit-transform':'rotate(-90deg)',
+	     '-o-transform':'rotate(-90deg)',
+	     '-ms-transform':'rotate(-90deg)',
+	     'transform':'rotate(-90deg)'
+		});		
 	}
 })
