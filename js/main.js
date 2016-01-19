5fdbba2e9c81536cfd6e9a641a3627e2881b8155
roberrrt-s <robert.spier@gmail.com>
2016-01-19 13:20:53 +0100

    jquery + image push

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-var calculate = function(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// hamburger menu
 
 var feed, tech, design = false;
 
@@ -8,10 +6,13 @@ $('#hamclick').click(function() {
 	if(feed) {
 		$('.content-section').show()
 		feed = false;
+		console.log("test")
+		this.src = 'img/hamburger-cross.png';
 	}
 	else {
 		$('.content-section').hide()
 		feed = true;
+		this.src = 'img/hamburger.png';
 	}
 })
 
@@ -19,10 +20,24 @@ $('#techclick').click(function() {
 	if(tech) {
 		$('.tech').show()
 		tech = false;
+		$('#techclick').css({
+	     '-moz-transform':'rotate(0deg)',
+	     '-webkit-transform':'rotate(0deg)',
+	     '-o-transform':'rotate(0deg)',
+	     '-ms-transform':'rotate(0deg)',
+	     'transform':'rotate(0deg)'
+		});
 	}
 	else {
 		$('.tech').hide()
 		tech = true;
+		$('#techclick').css({
+	     '-moz-transform':'rotate(-90deg)',
+	     '-webkit-transform':'rotate(-90deg)',
+	     '-o-transform':'rotate(-90deg)',
+	     '-ms-transform':'rotate(-90deg)',
+	     'transform':'rotate(-90deg)'
+		});
 	}
 })
 
@@ -30,9 +45,54 @@ $('#designclick').click(function() {
 	if(design) {
 		$('.design').show()
 		design = false;
+		$('#designclick').css({
+	     '-moz-transform':'rotate(0deg)',
+	     '-webkit-transform':'rotate(0deg)',
+	     '-o-transform':'rotate(0deg)',
+	     '-ms-transform':'rotate(0deg)',
+	     'transform':'rotate(0deg)'
+		});
 	}
 	else {
 		$('.design').hide()
 		design = true;
+		$('#designclick').css({
+	     '-moz-transform':'rotate(-90deg)',
+	     '-webkit-transform':'rotate(-90deg)',
+	     '-o-transform':'rotate(-90deg)',
+	     '-ms-transform':'rotate(-90deg)',
+	     'transform':'rotate(-90deg)'
+		});		
 	}
 })
+
+// menu
+
+$('#your-feed').click(function() {
+	$("#menu-arrow-container").css("margin-left", "0%")
+	$("#menu-arrow").css("border-top", "15px solid #3ab34d")
+})
+$('#trending').click(function() {
+	$("#menu-arrow-container").css("margin-left", "20%")
+	$("#menu-arrow").css("border-top", "15px solid #f4f8fb")
+})
+$('#recently-added').click(function() {
+	$("#menu-arrow-container").css("margin-left", "40%")
+	$("#menu-arrow").css("border-top", "15px solid #f4f8fb")
+})
+$('#design-menu').click(function() {
+	$("#menu-arrow-container").css("margin-left", "60%")
+	$("#menu-arrow").css("border-top", "15px solid #f4f8fb")
+})
+$('#tech-menu').click(function() {
+	$("#menu-arrow-container").css("margin-left", "80%")
+	$("#menu-arrow").css("border-top", "15px solid #f4f8fb")
+})
+
+// search
+
+$('.search-cross').click(function() {
+	$(this).removeClass("search-cross");
+	var remove = $(this).attr('class');
+	$('section.' + remove).remove();
+});
